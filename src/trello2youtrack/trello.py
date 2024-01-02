f875576f121d450006d28af1b2b3464a7ff82431
Shayan Amani <7shayan7@gmail.com>
2024-01-02 18:17:37 -0500

    Load Trello board

--- src/trello2youtrack/trello.py
+++ src/trello2youtrack/trello.py
@@ -1,8 +1,20 @@
+import json
+import os
+
+from typing import Union
+
+
 class Trello:
-    def __init__(self, board_json_path: str):
+    def __init__(self, board_json_path: Union[str, bytes, os.PathLike]):
         """
         Adapter class for Trello.
 
         :param board_json_path: File path to the Trello board JSON file
         """
         self.board_json_path = board_json_path
+
+        self.board = self.load_board()
+
+    def load_board(self):
+        with open(self.board_json_path, 'rb') as file_handle:
+            return json.load(file_handle)
