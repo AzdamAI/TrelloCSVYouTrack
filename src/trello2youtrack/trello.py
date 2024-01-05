ffc51a65410deacf21759047f101ea7a24214f14
Shayan Amani <7shayan7@gmail.com>
2024-01-04 22:14:52 -0500

    Make request method in Trello

--- src/trello2youtrack/trello.py
+++ src/trello2youtrack/trello.py
@@ -1,3 +1,6 @@
+import requests
+
+
 class Trello:
     def __init__(self, api_base_url: str = 'https://api.trello.com/1',
                  api_key: str = None, api_token=None) -> None:
@@ -15,3 +18,11 @@ class Trello:
         self.api_token = api_token
         if not api_base_url or not api_key or not api_token:
             raise ValueError('Base URL, API Key, and API token are required')
+
+    def request(self, method: str, url: str, **kwargs) -> requests.Response:
+        kwargs['params'] = {'key': self.api_key,
+                            'token': self.api_token,
+                            **kwargs.get('params', {})}
+        return requests.request(method,
+                                f'{self.api_base_url}{url}',
+                                **kwargs)
