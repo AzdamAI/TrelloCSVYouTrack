cf97b3fad9811dd07c0e88598e2ee4af708e6633
Shayan Amani <7shayan7@gmail.com>
2024-01-05 01:35:52 -0500

    Bump up version

--- scripts/importer.py
+++ scripts/importer.py
@@ -5,6 +5,7 @@ from src.trello2youtrack.youtrack import YouTrack
 
 TRELLO_API_KEY = os.getenv('TRELLO_API_KEY')
 TRELLO_API_TOKEN = os.getenv('TRELLO_API_TOKEN')
+TRELLO_BOARD_ID = os.getenv('TRELLO_BOARD_ID')
 
 YOUTRACK_API_BASE_URL = os.getenv('YOUTRACK_API_BASE_URL')
 YOUTRACK_PERM_TOKEN = os.getenv('YOUTRACK_PERM_TOKEN')
@@ -12,6 +13,9 @@ YOUTRACK_PERM_TOKEN = os.getenv('YOUTRACK_PERM_TOKEN')
 
 def main():
     trello = Trello(api_key=TRELLO_API_KEY, api_token=TRELLO_API_TOKEN)
+    card_powerups = trello.get_board_cards_powerups(TRELLO_BOARD_ID)
+    trello.export_board_story_points(card_powerups, 'export.csv')
+
     youtrack = YouTrack(api_base_url=YOUTRACK_API_BASE_URL,
                         perm_token=YOUTRACK_PERM_TOKEN)
 
